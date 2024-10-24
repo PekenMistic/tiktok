77be6fb0c585271b640af24300bada2cf9e7d86c
PekenMistic <panggungsandiwara589@gmail.com>
2024-10-24 20:49:04 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,11 @@ export default function RootLayout({
             `,
           }}
         />
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-M75R3SVR');</script>
       </head>
       <body className={inter.className}>
         <ThemeProvider
@@ -48,14 +53,10 @@ export default function RootLayout({
             <Footer />
           </div>
         </ThemeProvider>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=G-38YVB9VHYH"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
+        <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-M75R3SVR"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
       </body>
     </html>
   )
